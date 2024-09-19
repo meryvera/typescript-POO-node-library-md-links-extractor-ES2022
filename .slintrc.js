5b39ed38541bf8743075a3dc8c13a547d0ce6d94
mery vera <meryvera@merys-MacBook-Pro.local>
2024-09-19 15:09:29 -0500

    Boilerplate setup project

---
+++ .slintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    // Añade reglas personalizadas aquí
+  },
+};
